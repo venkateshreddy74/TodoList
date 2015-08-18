6a765dd92929c69d740f96bda90a19d95eac7d79
Venkateshreddy74 <vzb0010@auburn.edu>
2015-08-17 22:49:20 -0500

    Add  Initial UI activities and fragments along with navigation

--- app/src/main/java/com/example/venkatesh/todolist/interfaces/DateandTimeFragmentCommunicator.java
+++
@@ -1,9 +0,0 @@
-package com.example.venkatesh.todolist.interfaces;
-
-/**
- * Created by Venkatesh on 6/30/2015.
- */
-public interface DateandTimeFragmentCommunicator {
-
-    public void loadTimeFragment();
-}
